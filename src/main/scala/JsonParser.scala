a7a34b9e9e50387f9527a4ebd281262a3fe041e7
Pierre <38128948+PierreKieffer@users.noreply.github.com>
2019-09-24 16:06:55 +0200

    Improve jsonparser for simple and multi struct

--- src/main/scala/JsonParser.scala
+++ src/main/scala/JsonParser.scala
@@ -3,9 +3,38 @@ import scala.util.parsing.json._
 
 
 object JsonParser {
+  
+  case class SimpleStructJsonParserData(field1 : String, field2 : String)
 
-  /** ---------------------------------------
-    * Example of method to parse simple json
+  def SimpleStructJsonParser (JsonDataFile : String) : SimpleStructJsonParserData = {
+
+    /** ---------------------------------------
+      * Example of method to parse simple json structure 
+        {
+        "field1": "value",
+        "field2": "value",
+        "field3": "value"
+        }
+      }*/
+      
+    val JsonData : String = Source.fromFile(JsonDataFile).getLines.mkString
+    val jsonFormatData = JSON.parseFull(JsonData).map{
+      case json : Map[String,Any] =>
+        SimpleStructJsonParserData(json("field1").toString,json("field2").toString)
+    }.get
+    jsonFormatData
+  }
+
+  /*
+  ___________________________________________________________________________________
+ */
+  
+  case class ListStructJsonParserData(field1 : String, field2: String, field3 : String)
+
+  def ListStructJsonParser(JsonDataFile : String) : List[ListStructJsonParserData] = {
+
+    /** ---------------------------------------
+      * Example of method to parse json list structure
         {
         "fields": [
           {
@@ -16,23 +45,26 @@ object JsonParser {
         ]
       }*/
 
-  case class outputData(field1 : String, field2: String, field3 : String)
-
-  def singleMapJsonParser(JsonDataFile : String) : List[outputData] = {
-
     val JsonData : String = Source.fromFile(JsonDataFile).getLines.mkString
 
     val jsonFormatData = JSON.parseFull(JsonData).map{
       case json : Map[String, List[Map[String,String]]] =>
-        json("fields").map(v => outputData(v("field1"),v("field2"),v("field3")))
+        json("fields").map(v => ListStructJsonParserData(v("field1"),v("field2"),v("field3")))
     }.get
-
     jsonFormatData
   }
+  
+  /*
+  ___________________________________________________________________________________
+   */
+  
+  case class multiStructJsonParser(field1 : String, exampleClassData : List[elementClass])
+  case class elementClass(element1 : String, element2 : String)
+  
+  def multiStructJsonParser(jsonDataFile : String) : List[multiStructJsonParser] = {
 
-
-  /** ---------------------------------------
-    * Example of method to parse more complex json (multiple list of elements for example)
+    /** ---------------------------------------
+      * Example of method to parse json with multiple structures
            {
             "fields": [
               {
@@ -51,24 +83,17 @@ object JsonParser {
             ]
           }*/
 
-  case class elementClass(element1 : String, element2 : String)
-  case class outputDataClass(field1 : String, exampleClassData : List[elementClass])
-
-  def multipleMapJsonParser(jsonDataFile : String) : List[outputDataClass] = {
-
     val JsonData : String = Source.fromFile(jsonDataFile).getLines.mkString
     val jsonFormatData = JSON.parseFull(JsonData)
       .map{
         case json : Map[String, List[Map[String,Any]]] => json("fields").map(
           jsonElem =>
-            outputDataClass(jsonElem("field1").toString,
+            multiStructJsonParser(jsonElem("field1").toString,
               jsonElem("field2").asInstanceOf[List[Map[String,String]]].map{
                 case element : Map[String,String] => elementClass(element("field21"),element("field22"))
               })
         )
       }.get
-
     jsonFormatData
-
   }
 }
